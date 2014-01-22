53a52c4e6b84e4762388a01ea9fab5e46c7ae90e
Ville <ville@growthstories.com>
2014-01-22 23:28:18 +0200

    updated production bootstrap

--- GrowthStories.UI.WindowsPhone/Config/Bootstrap.Production.cs
+++ GrowthStories.UI.WindowsPhone/Config/Bootstrap.Production.cs
@@ -1,5 +1,9 @@
 ﻿
 using Growthstories.Core;
+using Growthstories.Sync;
+using Growthstories.UI.Services;
+using Growthstories.UI.ViewModel;
+using System;
 namespace Growthstories.UI.WindowsPhone
 {
     public class BootstrapProduction : Bootstrap
@@ -22,18 +26,14 @@ namespace Growthstories.UI.WindowsPhone
         protected override void AppConfiguration()
         {
             Bind<IIAPService>().To<GSIAP>().InSingletonScope();
+            Bind<IScheduleService>().To<GardenScheduler>()
+                .InSingletonScope()
+                .WithConstructorArgument(new TimeSpan(0, 0, 10));
+
+            Bind<ISynchronizer>().To<AutoSyncingSynchronizer>();
 
         }
 
-        //protected override void LogConfiguration()
-        //{
-        //    GSRemoteLog.Host = LOGHOST;
-        //    GSRemoteLog.Port = LOGPORT;
-
-        //    RxUIResolver.Register(() => GSRemoteLog.Instance, typeof(ILogger));
-        //    LogFactory.BuildLogger = type => new GSRemoteLog(type);
-        //    GSLogFactory.BuildLogger = type => new GSRemoteLog(type);
-        //}
 
         protected override void HttpConfiguration(string host = "default.lan", int port = 80)
         {
